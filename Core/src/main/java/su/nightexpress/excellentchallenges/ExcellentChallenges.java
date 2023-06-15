0067f49f05d93314bb05504a6651ccfc4a97d1bb
nulli0n <glitchy.cat@ya.ru>
2023-06-15 13:02:02 +0500

    2.0.8

--- Core/src/main/java/su/nightexpress/excellentchallenges/ExcellentChallenges.java
+++ Core/src/main/java/su/nightexpress/excellentchallenges/ExcellentChallenges.java
@@ -73,6 +73,7 @@ public class ExcellentChallenges extends NexPlugin<ExcellentChallenges> implemen
             case V1_19_R1 -> new V1_19_R1();
             case V1_19_R2 -> new V1_19_R2();
             case V1_19_R3 -> new V1_19_R3();
+            case V1_20_R1 -> new V1_20_R1();
         };
         return true;
     }
---
+++ V1_20_R1/src/main/java/su/nightexpress/excellentchallenges/nms/V1_20_R1.java
@@ -0,0 +1,35 @@
+package su.nightexpress.excellentchallenges.nms;
+
+import net.minecraft.core.BlockPosition;
+import net.minecraft.core.NonNullList;
+import net.minecraft.world.level.block.entity.TileEntity;
+import net.minecraft.world.level.block.entity.TileEntityBrewingStand;
+import org.bukkit.block.BrewingStand;
+import org.bukkit.craftbukkit.v1_20_R1.CraftWorld;
+import org.jetbrains.annotations.NotNull;
+import su.nexmedia.engine.utils.Reflex;
+
+import java.lang.reflect.Method;
+
+public class V1_20_R1 extends ChallengeNMS {
+
+    private static final Method BREWING_A = Reflex.getMethod(TileEntityBrewingStand.class, "a", NonNullList.class);
+
+    @Override
+    public boolean canBrew(@NotNull BrewingStand stand) {
+        if (BREWING_A == null) return true;
+
+        CraftWorld craftWorld = (CraftWorld) stand.getWorld();
+        TileEntity tile = craftWorld.getHandle().c_(new BlockPosition(stand.getX(), stand.getY(), stand.getZ()));
+        if (tile == null) return false;
+
+        //if (!(tile instanceof TileEntityBrewingStand)) return false;
+        //TileEntityBrewingStand te = (TileEntityBrewingStand) tile;
+
+        NonNullList<?> list = (NonNullList<?>) Reflex.getFieldValue(tile, "m");
+        if (list == null) return false;
+
+        Object val = Reflex.invokeMethod(BREWING_A, null, list);
+        return val != null && (boolean) val;
+    }
+}
